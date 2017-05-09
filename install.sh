b5672e64078b2ae0248f05167962103802d4b959
Haze <admin@haze.pw>
2017-05-08 23:23:43 -0400

    added concurrency for speed and fixed install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
+pip3 install -r requirements.txt
 pip3 install Nuitka
 nuitka --show-progress --lto --remove-output src/shitpost.py
-sudo mv shitpost.exe /usr/local/bin/shitpost
+mv shitpost.exe /usr/local/bin/shitpost
 echo 'done'
--- src/shitpost.py
+++ src/shitpost.py
@@ -1,3 +1,4 @@
+import concurrent.futures
 import argparse
 import sys
 from random import randint
@@ -33,6 +34,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument('text', nargs='*',type=str, help='text to intersperse.')
 parser.add_argument('-mi', '--min-emojis', default=2, type=int, help='minimum amount of emojis to be interspersed.')
 parser.add_argument('-ma', '--max-emojis', default=3, type=int, help='maximum amount of emojis to be interspersed.')
+parser.add_argument('-mwi', '--min-words', default=1, type=int, help='minimum amount of words to be split by.')
+parser.add_argument('-mwa', '--max-words', default=3, type=int, help='maximum amount of words to be split by.')
 args = parser.parse_args() 
 
 base = ' '.join(args.text)
@@ -40,15 +43,24 @@ if not base:
 	print('no input found.')
 	exit(1)
 
+if args.min_words < 1:
+	print('min words invalid range! valid: [1:]')
+	exit(1)
+
+if args.max_words < 1:
+	print('max words invalid range! valid [1:]')
+	exit(1)
+
 if args.min_emojis < 1 or args.min_emojis > 10:
-	print('min emojis valid range [1-10]')
+	print('min emojis invalid range! valid: [1-10]')
 	exit(1)
 
 if args.max_emojis > 10 or args.max_emojis < 1:
-	print('max emojis valid range [1-10].')
+	print('max emojis invalid range! valid: [1-10].')
 	exit(1)
 
-
-
 split_base = split_by_random(base, ' ', args.min_emojis, args.max_emojis)
-print(' '.join(list(map(emojify, split_base))))
+executor = concurrent.futures.ProcessPoolExecutor(len(split_base))
+futures = [executor.submit(emojify, section) for section in split_base]
+concurrent.futures.wait(futures)
+print(' '.join(list(map(lambda x: x.result(), futures))))
